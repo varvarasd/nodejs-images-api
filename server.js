5e38f4b839010841390c61aedd102e3e31adfffd
varvarasd <silviudanielvarvara@gmail.com>
2021-02-27 10:15:08 +0000

    initalise images api

--- server.js
+++ server.js
@@ -1,21 +1,37 @@
 const express = require('express');
 const cors = require('cors');
-const images = require('./images.json');
+const favicon = require('serve-favicon');
 const server = express();
+const rateLimit = require("express-rate-limit");
+require('dotenv').config();
+const images = require('./images');
 
-server.use(cors())
+server.use(cors());
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+    message:
+    "Too many requests from this IP, please try again after 15 minutes"
+});
+
+server.use('*', limiter);
+
+
+server.use(favicon(__dirname + '/favicon.ico'));
 
 server.use('*', (req, res, next) => {
-    if(req.headers.authorization !== 'mysecrettoken') {
-        res.status(403).send('Not authorised')
+    if(req.headers.authorization !== process.env.ACCESS) {
+        res.status(403).send('Not authorised').end()
+    } else {
+        next();
     }
-    next();
 });
 
 server.get("/", (req, res) => {
     res.send(images);
 });
 
-const port = 2000;
+const port = process.env.PORT || 2000;
 
 server.listen(port, () => console.log('APP LISTENING ON PORT: ' + port))
